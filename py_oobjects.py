c6ca32e53b3ac2f6f64a9ed784c4666630fca552
Sero95 <taisirsammour95@gmail.com>
2024-07-04 18:29:42 +0300

    add objects code simple example

---
+++ py_oobjects.py
@@ -0,0 +1,15 @@
+## Every thing is objects
+
+
+x = 5  # object from class int
+print(type(x))  # check output
+
+y = "Taisir"
+y.upper()  # this function icluded in str class and so on
+print(y.upper())
+
+
+z = [1, 2, 3, 4, 5, 6]
+print(z)
+z.append(7)
+print(z)
